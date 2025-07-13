11ef789aab8c80e5f78e077e2390266a44219ed5
gyuri <rbfl7645@naver.com>
2025-07-13 18:17:08 +0900

    prettier, eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,24 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import react from 'eslint-plugin-react';
-import prettier from 'eslint-config-prettier';
+import babelParser from '@babel/eslint-parser';
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 'latest',
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+      ecmaVersion: 2022,
       sourceType: 'module',
-      globals: globals.browser,
     },
     plugins: {
-      react,
+      react: (await import('eslint-plugin-react')).default,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.flat.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
+      // ...
     },
     settings: {
       react: {
@@ -26,6 +26,4 @@ export default [
       },
     },
   },
-
-  prettier,
 ];
