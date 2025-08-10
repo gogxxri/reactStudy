e218188bec78c21291952014436096d634b0de9e
gyuri <rbfl7645@naver.com>
2025-08-10 15:33:46 +0900

    styled-compponent : 가상선택자, 중첩규칙 등

--- styling/src/components/AuthInputs.jsx
+++ styling/src/components/AuthInputs.jsx
@@ -31,6 +31,20 @@ const Input = styled.input`
     0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -64,12 +78,8 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button type="button">Create a new account</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
